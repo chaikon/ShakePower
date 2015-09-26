f12c61d51fd9ddc92a3a011084ff151ba2bbf8ee
chaikon <achai@andrew.cmu.edu>
2015-09-26 01:40:26 -0400

    Initial Commit

---
+++ ShakePower/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShakePower
+//
+//  Created by Alexander Chai on 9/26/15.
+//  Copyright © 2015 AZALSAR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ShakePower/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShakePower
+//
+//  Created by Alexander Chai on 9/26/15.
+//  Copyright © 2015 AZALSAR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
