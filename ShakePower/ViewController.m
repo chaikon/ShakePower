c4d923661c1da2da2b28ba7e9c7a6f8129fa3c30
chaikon <achai@andrew.cmu.edu>
2015-09-26 23:27:39 -0400

    changed somethings here and there

--- ShakePower/ViewController.m
+++ ShakePower/ViewController.m
@@ -14,14 +14,36 @@
 
 @implementation ViewController
 
+AVAudioPlayer *sound;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"beisd" ofType:@"wav"];
+    NSLog(path);
+    NSURL *soundUrl = [NSURL fileURLWithPath:path];
+    sound = [[AVAudioPlayer alloc] initWithContentsOfURL:soundUrl error:nil];
+//    sound.numberOfLoops=-1;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)motionBegan:(UIEventSubtype)motion withEvent:(UIEvent *)event{
+    if (motion==UIEventSubtypeMotionShake){
+        [sound stop];
+        NSLog(@"playstart");
+        [sound play];
+    }
+}
+
+-(void)motionEnded:(UIEventSubtype)motion withEvent:(UIEvent *)event{
+    if (motion==UIEventSubtypeMotionShake){
+        NSLog(@"playstop");
+        [sound stop];
+    }
 }
 
 @end
